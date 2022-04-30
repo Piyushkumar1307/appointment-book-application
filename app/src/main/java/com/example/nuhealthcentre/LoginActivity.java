2c91f772253a20324c84e98e7f65c96dea137261
Piyush Kumar <sonu788380@gmail.com>
2022-04-30 20:24:58 +0530

    appointment booking application

--- app/src/main/java/com/example/nuhealthcentre/LoginActivity.java
+++ app/src/main/java/com/example/nuhealthcentre/LoginActivity.java
@@ -1,85 +1,104 @@
 package com.example.nuhealthcentre;
 
-import androidx.annotation.NonNull;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
-    private EditText email, password;
-    private Button btnLogin;
-    private TextView textRegister;
+
+    private Button btnAdmin;
+    ImageView google_img;
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mAuth = FirebaseAuth.getInstance();
-        email = findViewById(R.id.login_email);
-        password = findViewById(R.id.login_password);
-        btnLogin = findViewById(R.id.login);
-        textRegister = findViewById(R.id.text_register);
+        //TO HIDE ACTION BAR AND STATUS BAR
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        google_img = findViewById(R.id.googlelogin);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        gsc = GoogleSignIn.getClient(this,gso);
+
+        google_img.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                login();
+                SignIn();
             }
         });
 
-        textRegister.setOnClickListener(new View.OnClickListener() {
+        //btn
+        btnAdmin = findViewById(R.id.loginAdmin);
+
+        btnAdmin = findViewById(R.id.loginAdmin);
+        btnAdmin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                openAdminActivity();
             }
         });
-    }
-
-    private void login() {
-        String user = email.getText().toString().trim();
-        String pass = password.getText().toString().trim();
-        if (pass.isEmpty())
-        {
-            email.setError("Email can not be empty");
-        }
-        if (pass.isEmpty())
-        {
-            password.setError("Password can not be empty");
-        }
-        else
-        {
-            mAuth.signInWithEmailAndPassword(user,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task)
-                {
-                    if(task.isSuccessful())
-                    {
-                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(LoginActivity.this , MainActivity.class));
-                    }
-                    else
-                    {
-                        Toast.makeText(LoginActivity.this, "Login Failed"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-
-                }
-            });
-        }
 
     }
+
+    public void openAdminActivity(){
+        Intent intent = new Intent(this, AdminActivity.class);
+        startActivity(intent);
+    }
+
+    private void SignIn() {
+
+        Intent intent = gsc.getSignInIntent();
+        startActivityForResult(intent,100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==100){
+            Task<GoogleSignInAccount> task=GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                task.getResult(ApiException.class);
+                HomeActivity();
+            } catch (ApiException e) {
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void HomeActivity() {
+
+        finish();
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+    }
+
+
+
 
 
 }
